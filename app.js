38529a1c42c1cfa94929c2d7ea8dc70bf77a7c64
George Higbie <gehigbie@gmail.com>
2017-03-20 21:21:00 -0700

    add app.get to app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const ejs     = requier("ejs");
 const app     = express();
+
+
+app.get("*", function(req, res){
+    res.render("notfound");
+});
+
+app.listen()
